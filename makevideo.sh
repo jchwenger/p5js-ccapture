1085165202fa08f9503b929347dba0cf1cee039c
jchwenger <34098722+jchwenger@users.noreply.github.com>
2023-08-29 16:33:11 +0100

    makevideo | prettier

--- makevideo.sh
+++ makevideo.sh
@@ -6,4 +6,11 @@ fps=30
 frameName="%07d.png"
 crf=17 # CRF should be between 17 and 28. Lower is higher quality
 
-ffmpeg -r $fps -f image2 -s $dimensions -i $frameName -vcodec libx264 -crf $crf  -pix_fmt yuv420p $outputName
+ffmpeg \
+  -r $fps \
+  -f image2 \
+  -s $dimensions \
+  -i $frameName \
+  -vcodec libx264 \
+  -crf $crf \
+  -pix_fmt yuv420p $outputName
